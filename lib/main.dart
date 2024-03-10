0e90233d9f03dec9432bd6074dfdbbffaa563ed5
UmairMarrie <dapp5552@gmail.com>
2024-03-10 14:29:23 +0500

    inital commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:first_app/home_Page.dart';
+
+import 'package:first_app/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: SplashScreen(),
     );
   }
 }
